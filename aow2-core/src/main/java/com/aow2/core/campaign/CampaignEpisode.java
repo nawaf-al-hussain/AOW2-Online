fa7e0ea207c1d82eb9e5ff7f032aaaea870caec9
Nawaf Al Hussain Khondokar <nkhondokar2420136@bscse.uiu.ac.bd>
2026-06-13 09:01:54 +0000

    fix: add title(), faction(), CUSTOM_MISSIONS to CampaignEpisode enum
    - Added title() and faction() accessor methods
    - Added CUSTOM_MISSIONS enum value used by CampaignManager
    - Added Faction import

--- aow2-core/src/main/java/com/aow2/core/campaign/CampaignEpisode.java
+++ aow2-core/src/main/java/com/aow2/core/campaign/CampaignEpisode.java
@@ -1,13 +1,32 @@
 package com.aow2.core.campaign;
 
+import com.aow2.common.model.Faction;
+
 /**
  * Campaign episodes in Art of War 2: Online.
  * REF: campaign_guide.md - Episode 1 (Global Confederation) and Episode 2 (Liberation of Peru)
  */
 public enum CampaignEpisode {
     /** Episode 1: Global Confederation - 14 missions playing as the Confederation faction. */
-    GLOBAL_CONFEDERATION,
+    GLOBAL_CONFEDERATION("Global Confederation", Faction.CONFEDERATION, 14),
 
     /** Episode 2: Liberation of Peru - 15 missions playing as the Resistance faction. */
-    LIBERATION_OF_PERU
+    LIBERATION_OF_PERU("Liberation of Peru", Faction.RESISTANCE, 15),
+
+    /** Custom missions loaded from mods. */
+    CUSTOM_MISSIONS("Custom Missions", Faction.NEUTRAL, 0);
+
+    private final String title;
+    private final Faction faction;
+    private final int missionCount;
+
+    CampaignEpisode(String title, Faction faction, int missionCount) {
+        this.title = title;
+        this.faction = faction;
+        this.missionCount = missionCount;
+    }
+
+    public String title() { return title; }
+    public Faction faction() { return faction; }
+    public int missionCount() { return missionCount; }
 }
